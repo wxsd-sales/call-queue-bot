01f4b0e0bc359dc0ccd2eca1400828530affecc7
rajitha123k <rajithabhavani.kantheti@gmail.com>
2022-07-21 17:09:02 -0400

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 
 import indexRouter from './routes/index.js';
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
